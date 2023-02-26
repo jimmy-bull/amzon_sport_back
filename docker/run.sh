5f149fa5267072607887e5bdefe6687557fbb3d7
jimmy-bull <jbull635@gmail.com>
2023-02-26 21:25:30 +0100

    removed nginx config

--- docker/run.sh
+++ docker/run.sh
@@ -6,4 +6,4 @@ cd /var/www
 php artisan cache:clear
 php artisan route:cache
 php artisan optimize:clear && php artisan optimize && php artisan storage:link && php -d memory_limit=-1 artisan migrate:fresh --seed
-/usr/bin/supervisord -c /etc/supervisord.conf
+# /usr/bin/supervisord -c /etc/supervisord.conf
